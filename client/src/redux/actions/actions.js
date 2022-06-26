a7648ee6cea6c2ddb613bd195a100a23733b4fbf
Your Name <you@example.com>
2022-06-25 23:08:58 -0300

    actions

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -21,6 +21,9 @@ export const GET = 'GET'
 //const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001'
 const API_URL = 'https://sergiopokemonapi.herokuapp.com'
 
+
+
+
 //Action para traernos todos los pokemons
 
 
